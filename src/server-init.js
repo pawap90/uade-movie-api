fdf13cfba72e2a0a716f4fcc4f0b647a7e4317ba
paulasantamaria <m_paula_santamaria@live.com>
2020-06-20 22:24:05 -0300

    Lint fixes

--- src/server-init.js
+++ src/server-init.js
@@ -14,8 +14,8 @@ module.exports.startServer = async () => {
 
     // Set CORS
     app.use('/', function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With');
         next();
     });
 
