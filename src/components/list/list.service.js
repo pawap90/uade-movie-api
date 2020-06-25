9bba678bea4a8789b6b3562e7f73520d3a4f5534
jaime-human <jaime99_og@hotmail.com>
2020-06-24 22:40:28 -0300

    fixes applied over PR comments

--- src/components/list/list.service.js
+++ src/components/list/list.service.js
@@ -6,13 +6,12 @@ const mongoose = require('mongoose');
 /**
  * Post list
  * @param {Object} list accountId, isDefault, name, isPublic & mediaItems
- * @throws {Unauthorized} When credentials are wrong or not provided
  * @throws {InternalServerError} When there's an unhandled error.
  */
 module.exports.postList = async (list) => {
     list.accountId = mongoose.Types.ObjectId(list.accountId);
 
-    listModel.create(list);
+    await listModel.create(list);
 };
 
 /**
