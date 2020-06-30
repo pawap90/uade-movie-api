151b0961c6116f8b694b5ae2b7ee1943f87d20c5
paulasantamaria <m_paula_santamaria@live.com>
2020-06-30 07:39:56 -0300

    Get comments without auth

--- src/components/rate/rate.routes.js
+++ src/components/rate/rate.routes.js
@@ -12,6 +12,6 @@ router.post('/', authorize, rateController.postRate);
 /**
  * Gets Rates of a media item
  */
-router.get('/:mediaType/:mediaId', authorize, rateController.getAllRates);
+router.get('/:mediaType/:mediaId', rateController.getAllRates);
 
 module.exports = router;
