ce8273b14fef84147c65dee924888a0cde1e554f
paulasantamaria <m_paula_santamaria@live.com>
2020-06-30 00:47:17 -0300

    Return user complete name when retrieving rates

--- src/components/rate/rate.model.js
+++ src/components/rate/rate.model.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
  * Rate model schema.
  */
 const rateSchema = new mongoose.Schema({
-    accountId: { type: Object, required: true },
+    accountId: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'account',
+        required: true
+    },
     mediaType: { type: String, required: true },
     mediaId: { type: Number, required: true },
     rating: { type: Number, required: true },
