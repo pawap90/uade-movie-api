0d9cc70068ef7a507f29765b05dac2ad3301d43c
paulasantamaria <m_paula_santamaria@live.com>
2020-07-03 21:16:30 -0300

    Update model

--- src/components/employee/employee.model.js
+++ src/components/employee/employee.model.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const mongoose = require('mongoose');
 
 const personaModel = require('../commons/persona/persona.model');
@@ -7,12 +9,13 @@ const personaModel = require('../commons/persona/persona.model');
  */
 const employeeSchema = new mongoose.Schema({
     persona: { type: personaModel, required: false },
-    employeeNumber: { type: String, required: true }, // E.g: 0001
+    employeeNumber: { type: String, required: true }, // User friendly, 4 digit number - E.g: 0001
     phoneNumber: { type: String, required: true },
     cuit: { type: String, required: true },
     jobTitle: { type: String, required: true },
     grossSalary: { type: Number, required: true },
-    entryDate: { type: Date, required: true }
+    entryDate: { type: Date, required: true },
+    createDate: { type: Date, required: true, default: Date.now() }
 });
 
 module.exports = mongoose.model('employee', employeeSchema);
