fbe4520d427fe874ae67346e637e5217051140dd
jaime-human <jaime99_og@hotmail.com>
2020-07-07 03:03:22 -0300

    delete member by id is working

--- src/components/member/member.controller.js
+++ src/components/member/member.controller.js
@@ -57,3 +57,17 @@ module.exports.updateMedicalInfo = async (req, res, next) => {
         return next(err);
     }
 };
+
+/**
+ * Delete member by id
+ */
+module.exports.deleteById = async (req, res, next) => {
+    try {
+        await memberService.deleteById(req.params.id)
+
+        return res.json('Success!');
+    }
+    catch (err) {
+        return next(err);
+    }
+};
