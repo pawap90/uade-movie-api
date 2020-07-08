fba2f65a482ab55266db8a4b6e46bc48495b61da
paulasantamaria <m_paula_santamaria@live.com>
2020-07-08 19:40:10 -0300

    Add space

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ serverInit.startServer()
     .then(app => {
         const port = process.env.CUSTOM_PORT || process.env.PORT;
         app.listen(port);
+
         console.log(`Listening on port ${port}`);
     })
     .catch((err) => console.log(err));
