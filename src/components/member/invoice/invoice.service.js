71fe9c495979c71cf3e2e4b8a1e098396826eb3a
jaime-human <jaime99_og@hotmail.com>
2020-07-25 03:31:11 -0300

    invoice model created

---
+++ src/components/member/invoice/invoice.service.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const error = require('throw.js');
+
+
+
+/**
+ * Creates a new invoice
+ * @param {String} invoice Member identifier
+ * @throws {InternalServerError} When there's an unexpected error.
+ */
+module.exports.create = async (invoice) => {
+    try {
+
+    }
+    catch (err) {
+        throw err;
+    }
+};
