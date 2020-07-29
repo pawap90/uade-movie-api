b1cb085ca8f63ff16c576e8312994fc40bcfc7e4
Paula Santamaría <m_paula_santamaria@live.com>
2020-07-28 23:52:39 -0300

    lint fixes

--- src/external-services/external-service-logger.js
+++ src/external-services/external-service-logger.js
@@ -3,11 +3,11 @@ module.exports.logResponse = async (serviceName, method, res, params) => {
     const auxRes = res.clone();
     const result = await auxRes.json();
     logRes(serviceName, method || 'GET', auxRes.url, params, auxRes.status, auxRes.statusText, result);
-}
+};
 
 module.exports.logError = (serviceName, method, url, params, err) => {
     logRes(serviceName, method, url, params, err.statusCode, err.name, err);
-}
+};
 
 const logRes = (serviceName, method, endpoint, params, status, statusText, result) => {
     console.log(`${serviceName}-----------------------------`);
@@ -15,15 +15,15 @@ const logRes = (serviceName, method, endpoint, params, status, statusText, resul
     console.log(`Endpoint: ${endpoint}`);
 
     if (params) {
-        console.log(`Params:`);
+        console.log('Params:');
         console.log(params);
     }
 
     if (result) {
-        console.log(`Response:`);
+        console.log('Response:');
         console.log(result);
     }
 
     console.log(`Status: ${status} ${statusText}`);
-    console.log(`-----------------------------`);
-}
+    console.log('-----------------------------');
+};
